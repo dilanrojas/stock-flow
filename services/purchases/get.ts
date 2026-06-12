565a23e552ed50eeea2d6da5be39849248e50aec
Angely Castro <angely.castro@ucr.ac.cr>
2026-06-12 09:22:36 -0600

    fix: get

--- services/purchases/get.ts
+++ services/purchases/get.ts
@@ -1,6 +1,10 @@
-import type {  PurchaseResponseModel} from '../../lib/types/purchase';
+import type { PurchaseSummaryResponseModel, PurchaseResponseModel} from '../../lib/types/purchase';
 import type { PaginatedResponse } from '../../lib/types/page';
 import { getJSON } from '../api';
+
+// Obtener lista de compras (resumen)
+export const getPurchases = async (page = 0) =>
+  getJSON<PaginatedResponse<PurchaseSummaryResponseModel>>(`/purchases?page=${page}`);
 
 
 export const getPurchaseById = async (resourceId: string) =>
